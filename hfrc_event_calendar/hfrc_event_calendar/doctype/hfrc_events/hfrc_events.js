a1a05ca172369cf6a6674f5e59b12db8b1006878
Nikhil Kandekar <nikhilbkandekar@gmail.com>
2021-09-02 09:04:13 +0530

    added preview for 1

--- hfrc_event_calendar/hfrc_event_calendar/doctype/hfrc_events/hfrc_events.js
+++ hfrc_event_calendar/hfrc_event_calendar/doctype/hfrc_events/hfrc_events.js
@@ -1,6 +1,6 @@
 // Copyright (c) 2021, ppcrc and contributors
 // For license information, please see license.txt
-
+var count=1;
 frappe.ui.form.on('HFRC Events', {
 	category: function (frm) {
 		let category = frm.doc.category
@@ -16,12 +16,47 @@ frappe.ui.form.on('HFRC Events', {
 	location: function (frm) {
 		frm.toggle_display('other_location', frm.doc.location === 'Other')
 	},
-	onload_post_render: function (frm) {
-		// var count=0;
-		// console.log("Image Added");
-		// console.log(++count);
-		// console.log($('[data-fieldname="image"] > .form-clickable-section > .flex > .grid-buttons > .grid-add-row').text());
-		// $('[data-fieldname="image"] > .form-clickable-section > .flex > .grid-buttons > .grid-add-row').hide();
-		// testing done
+	onload_post_render: function (frm) {	
+		$('[data-fieldname="image1"]').find('.attached-file-link').bind("DOMSubtreeModified", function() {
+			let link1 = $('.attached-file-link').text();
+			console.log(link1+"?v="+Date());
+			console.log(".attached-file-link");
+			frm.refresh_field("preview1");			
+		});
+		
+
+	},
+	add_photo: function(frm){
+		if(count<5){
+			count++;
+			frm.toggle_display("image"+count,1);
+			frm.toggle_display("preview"+count,1);
+			if(count===5){
+				frm.toggle_display('add_photo',0);
+				frm.refresh_field("add_photo");
+			}
+			if(count===2){
+				frm.toggle_display('remove_photo',1);
+				frm.refresh_field("remove_photo");
+			}
+		}
+		console.log(count);
+	},
+	remove_photo:function(frm){
+		if(count>=2){
+			console.log("deleted",count);
+			frm.toggle_display("image"+count,0);
+			frm.toggle_display("preview"+count,0);
+			count--;
+			if(count===1){
+				frm.toggle_display('remove_photo',0);
+				frm.refresh_field("remove_photo");
+			}
+			if(count===4){
+				frm.toggle_display('add_photo',1);
+				frm.refresh_field("add_photo");
+			}
+		}
+		console.log(count);
 	}
 });
